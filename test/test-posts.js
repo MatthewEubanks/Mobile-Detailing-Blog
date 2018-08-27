c93493e4b641aa8c0e1fb481cd314a16a5cd5f85
Matt Eubanks <spc.eubanks.matthew@gmail.com>
2018-08-26 20:21:21 -0700

    tests

--- test/test-posts.js
+++ test/test-posts.js
@@ -1,115 +1,216 @@
-const chai = require("chai");
-const chaiHttp = require("chai-http");
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const faker = require('faker');
+const mongoose = require('mongoose');
 
-const expect = chai.expect;
+// this makes the should syntax available throughout
+// this module
+const should = chai.should();
 
-const { app, runServer, closeServer } = require("../server");
+const { BlogPost } = require('../models');
+const { closeServer, runServer, app } = require('../server');
+const { TEST_DATABASE_URL } = require('../config');
 
 chai.use(chaiHttp);
 
-describe("Blog Posts", function() {
-  before(function() {
-    return runServer();
+
+function tearDownDb() {
+  return new Promise((resolve, reject) => {
+    console.warn('Deleting database');
+    mongoose.connection.dropDatabase()
+      .then(result => resolve(result))
+      .catch(err => reject(err));
+  });
+}
+
+function seedBlogPostData() {
+  console.info('seeding blog post data');
+  const seedData = [];
+  for (let i = 1; i <= 10; i++) {
+    seedData.push({
+      title: faker.lorem.words(),
+      content: faker.lorem.paragraph(),
+      picture: faker.image.imageUrl(),
+      username: faker.internet.userName()
+    });
+  }
+  // this will return a promise
+  return BlogPost.insertMany(seedData);
+}
+
+
+describe('blog posts API resource', function () {
+
+  before(function () {
+    return runServer(TEST_DATABASE_URL);
   });
 
-  after(function() {
+  beforeEach(function () {
+    return seedBlogPostData();
+  });
+
+  afterEach(function () {
+    return tearDownDb();
+  });
+
+  after(function () {
     return closeServer();
   });
 
-  it("should list items on GET", function() {
-    return chai
-      .request(app)
-      .get("/posts")
-      .then(function(res) {
-        expect(res).to.have.status(200);
-        expect(res).to.be.json;
-        expect(res.body).to.be.a("array");
-        expect(res.body.length).to.be.above(0);
-        res.body.forEach(function(item) {
-          expect(item).to.be.a("object");
-          expect(item).to.have.all.keys(
-            "id",
-            "title",
-            "content",
-            "picture",
-            "username"
-          );
+  describe('GET endpoint', function () {
+
+    it('should return all existing posts', function () {
+
+      let res;
+      return chai.request(app)
+        .get('/posts')
+        .then(_res => {
+          res = _res;
+          res.should.have.status(200);
+          // otherwise our db seeding didn't work
+          res.body.should.have.lengthOf.at.least(1);
+
+          return BlogPost.count();
+        })
+        .then(count => {
+
+          res.body.should.have.lengthOf(count);
         });
-      });
-  });
+    });
 
-  it("should add a blog post on POST", function() {
-    const newPost = {
-      title: "Lorem ip some",
-      content: "foo foo foo foo",
-      picture: "https://pixabay.com/get/ea30b8092cfd073ed1584d05fb1d4e97e07ee3d21cac104496f5c278a1eeb5bb_340.jpg",
-      username: "demo@demo.com"
-    };
-    const expectedKeys = ["id"].concat(Object.keys(newPost));
+    it('should return posts with right fields', function () {
+      // Strategy: Get back all posts, and ensure they have expected keys
 
-    return chai
-      .request(app)
-      .post("/posts")
-      .send(newPost)
-      .then(function(res) {
-        expect(res).to.have.status(201);
-        expect(res).to.be.json;
-        expect(res.body).to.be.a("object");
-        expect(res.body).to.have.all.keys(expectedKeys);
-        expect(res.body.title).to.equal(newPost.title);
-        expect(res.body.content).to.equal(newPost.content);
-        expect(res.body.author).to.equal(newPost.author);
-        expect(res.body.username).to.equal(newPost.username);
-      });
-  });
+      let resPost;
+      return chai.request(app)
+        .get('/posts')
+        .then(function (res) {
 
-  it("should error if POST missing expected values", function() {
-    const badRequestData = {};
-    return chai
-      .request(app)
-      .post("/posts")
-      .send(badRequestData)
-      .then(function(res) {
-        expect(res).to.have.status(400);
-      });
-  });
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('array');
+          res.body.should.have.lengthOf.at.least(1);
 
-  it("should update blog posts on PUT", function() {
-    return (
-      chai
-        .request(app)
-        // first have to get
-        .get("/posts")
-        .then(function(res) {
-          const updatedPost = Object.assign(res.body[0], {
-            title: "connect the dots",
-            content: "la la la la la"
+          res.body.forEach(function (post) {
+            post.should.be.a('object');
+            post.should.include.keys('id', 'title', 'content', 'picture', 'username');
           });
-          return chai
-            .request(app)
-            .put(`/posts/${res.body[0].id}`)
-            .send(updatedPost)
-            .then(function(res) {
-              expect(res).to.have.status(204);
-            });
+          // just check one of the posts that its values match with those in db
+          // and we'll assume it's true for rest
+          resPost = res.body[0];
+          return BlogPost.findById(resPost.id);
         })
-    );
+        .then(post => {
+          resPost.title.should.equal(post.title);
+          resPost.content.should.equal(post.content);
+          resPost.picture.should.equal(post.picture);
+          resPost.username.should.equal(post.username);
+        });
+    });
   });
 
-  it("should delete posts on DELETE", function() {
-    return (
-      chai
-        .request(app)
-        // first have to get
-        .get("/posts")
-        .then(function(res) {
-          return chai
-            .request(app)
-            .delete(`/posts/${res.body[0].id}`)
-            .then(function(res) {
-              expect(res).to.have.status(204);
-            });
+  describe('POST endpoint', function () {
+    // strategy: make a POST request with data,
+    // then prove that the post we get back has
+    // right keys, and that `id` is there (which means
+    // the data was inserted into db)
+    it('should add a new blog post', function () {
+
+      const newPost = {
+        title: faker.lorem.words(),
+      content: faker.lorem.paragraph(),
+      picture: faker.image.imageUrl(),
+      username: faker.internet.userName()
+      };
+
+      return chai.request(app)
+        .post('/posts')
+        .send(newPost)
+        .then(function (res) {
+          res.should.have.status(201);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.include.keys(
+            'id', 'title', 'content', 'picture', 'username');
+          res.body.title.should.equal(newPost.title);
+          // cause Mongo should have created id on insertion
+          res.body.id.should.not.be.null;
+          res.body.picture.should.equal(
+            newPost.picture);
+          res.body.content.should.equal(newPost.content);
+          res.body.username.should.equal(newPost.username);
+          return BlogPost.findById(res.body.id);
         })
-    );
+        .then(function (post) {
+          post.title.should.equal(newPost.title);
+          post.content.should.equal(newPost.content);
+          post.picture.should.equal(newPost.picture);
+          post.username.should.equal(newPost.username);
+        });
+    });
+  });
+
+  describe('PUT endpoint', function () {
+
+    // strategy:
+    //  1. Get an existing post from db
+    //  2. Make a PUT request to update that post
+    //  4. Prove post in db is correctly updated
+    it('should update fields you send over', function () {
+      const updateData = {
+        title: 'cats cats cats',
+        content: 'dogs dogs dogs',
+        picture: faker.image.imageUrl()
+      };
+
+      return BlogPost
+        .findOne()
+        .then(post => {
+          updateData.id = post.id;
+
+          return chai.request(app)
+            .put(`/posts/${post.id}`)
+            .send(updateData);
+        })
+        .then(res => {
+          res.should.have.status(204);
+          return BlogPost.findById(updateData.id);
+        })
+        .then(post => {
+          post.title.should.equal(updateData.title);
+          post.content.should.equal(updateData.content);
+          post.picture.should.equal(updateData.picture);
+        });
+    });
+  });
+
+  describe('DELETE endpoint', function () {
+    // strategy:
+    //  1. get a post
+    //  2. make a DELETE request for that post's id
+    //  3. assert that response has right status code
+    //  4. prove that post with the id doesn't exist in db anymore
+    it('should delete a post by id', function () {
+
+      let post;
+
+      return BlogPost
+        .findOne()
+        .then(_post => {
+          post = _post;
+          return chai.request(app).delete(`/posts/${post.id}`);
+        })
+        .then(res => {
+          res.should.have.status(204);
+          return BlogPost.findById(post.id);
+        })
+        .then(_post => {
+          // when a variable's value is null, chaining `should`
+          // doesn't work. so `_post.should.be.null` would raise
+          // an error. `should.be.null(_post)` is how we can
+          // make assertions about a null value.
+          should.not.exist(_post);
+        });
+    });
   });
 });
